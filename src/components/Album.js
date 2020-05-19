ceee81b48823c336542538e6da2d8f95f2f98eb5
ATrovillion <aaron.trovillion@gmail.com>
2020-05-19 11:00:23 -0400

    Add completed buttons checkpoint work

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
     constructor(props) {
@@ -48,6 +49,14 @@ class Album extends Component {
         }
     }
 
+    handlePrevClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.max(0, currentIndex - 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+    }
+
     handleHover(song) {
         if (this.state.isHovered !== song) {
             this.setState({ isHovered: song })
@@ -55,8 +64,7 @@ class Album extends Component {
             this.setState({ isHovered: []} )
         } else {
             return
-        }
-        
+        }  
     }
 
     render() {
@@ -103,6 +111,12 @@ class Album extends Component {
                             })}
                     </tbody>
                 </table>
+                <PlayerBar
+                    isPlaying={this.state.isPlaying}
+                    currentSong={this.state.currentSong}
+                    handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+                    handlePrevClick={() =>this.handlePrevClick()}
+                    />
             </section>
         );
     }
