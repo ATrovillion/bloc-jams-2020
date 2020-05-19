01cb777ffd7c300453c86369d097a91f1f7e2e88
ATrovillion <aaron.trovillion@gmail.com>
2020-05-19 11:15:25 -0400

    Add completed buttons assignment work

--- src/components/Album.js
+++ src/components/Album.js
@@ -57,6 +57,14 @@ class Album extends Component {
         this.play();
     }
 
+    handleNextClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.min(this.state.album.songs.length - 1, currentIndex + 1);
+        const newSong = this.state.album.songs[newIndex]
+        this.setSong(newSong);
+        this.play();
+    }
+
     handleHover(song) {
         if (this.state.isHovered !== song) {
             this.setState({ isHovered: song })
@@ -115,7 +123,8 @@ class Album extends Component {
                     isPlaying={this.state.isPlaying}
                     currentSong={this.state.currentSong}
                     handleSongClick={() => this.handleSongClick(this.state.currentSong)}
-                    handlePrevClick={() =>this.handlePrevClick()}
+                    handlePrevClick={() => this.handlePrevClick()}
+                    handleNextClick={() => this.handleNextClick()}
                     />
             </section>
         );
